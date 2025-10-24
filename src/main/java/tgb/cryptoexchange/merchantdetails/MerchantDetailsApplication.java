3fd0881f7657d28fbf631d36b9dfb114dae1cd2b
barilo <e.barilo@isida.by>
2025-10-24 12:40:35 +0300

    Initial commit

---
+++ src/main/java/tgb/cryptoexchange/merchantdetails/MerchantDetailsApplication.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.merchantdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantDetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantDetailsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tgb/cryptoexchange/merchantdetails/MerchantDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.merchantdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantDetailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
