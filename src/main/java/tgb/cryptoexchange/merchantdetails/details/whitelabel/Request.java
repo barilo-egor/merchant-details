1551a90405c02112f3cacb0f1b68d190c44c7c06
barilo <e.barilo@isida.by>
2025-10-28 15:18:52 +0300

    удален билдер

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/whitelabel/Request.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/whitelabel/Request.java
@@ -1,15 +1,9 @@
 package tgb.cryptoexchange.merchantdetails.details.whitelabel;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class Request {
     private String type = "in";
 
