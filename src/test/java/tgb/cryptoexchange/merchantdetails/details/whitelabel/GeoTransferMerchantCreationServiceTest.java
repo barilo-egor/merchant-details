da556a84ed903a831c5ba825b82bdb33ae4e3cd5
barilo <e.barilo@isida.by>
2025-10-29 16:18:21 +0300

    удалены неиспользуемые моки

--- src/test/java/tgb/cryptoexchange/merchantdetails/details/whitelabel/GeoTransferMerchantCreationServiceTest.java
+++ src/test/java/tgb/cryptoexchange/merchantdetails/details/whitelabel/GeoTransferMerchantCreationServiceTest.java
@@ -1,32 +1,15 @@
 package tgb.cryptoexchange.merchantdetails.details.whitelabel;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.reactive.function.client.WebClient;
 import tgb.cryptoexchange.merchantdetails.enums.Merchant;
-import tgb.cryptoexchange.merchantdetails.properties.GeoTransferProperties;
-import tgb.cryptoexchange.merchantdetails.service.SignatureService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class GeoTransferMerchantCreationServiceTest {
-
-    @Mock
-    private WebClient webClient;
-
-    @Mock
-    private GeoTransferProperties geoTransferProperties;
-
-    @Mock
-    private ObjectMapper objectMapper;
-
-    @Mock
-    private SignatureService signatureService;
 
     @InjectMocks
     private GeoTransferMerchantCreationService geoTransferMerchantCreationService;
--- src/test/java/tgb/cryptoexchange/merchantdetails/details/whitelabel/RostrastMerchantCreationServiceTest.java
+++ src/test/java/tgb/cryptoexchange/merchantdetails/details/whitelabel/RostrastMerchantCreationServiceTest.java
@@ -1,32 +1,15 @@
 package tgb.cryptoexchange.merchantdetails.details.whitelabel;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.reactive.function.client.WebClient;
 import tgb.cryptoexchange.merchantdetails.enums.Merchant;
-import tgb.cryptoexchange.merchantdetails.properties.RostrastProperties;
-import tgb.cryptoexchange.merchantdetails.service.SignatureService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class RostrastMerchantCreationServiceTest {
-
-    @Mock
-    private WebClient webClient;
-
-    @Mock
-    private RostrastProperties rostrastProperties;
-
-    @Mock
-    private ObjectMapper objectMapper;
-
-    @Mock
-    private SignatureService signatureService;
 
     @InjectMocks
     private RostrastMerchantCreationService rostrastMerchantCreationService;
