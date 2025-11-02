15b350524c7482371b4d217d31f57b0a24a65db5
barilo <barilo>
2025-11-02 22:56:32 +0300

    MerchantResponse

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/appexbit/Response.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/appexbit/Response.java
@@ -2,15 +2,23 @@ package tgb.cryptoexchange.merchantdetails.details.appexbit;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import tgb.cryptoexchange.merchantdetails.details.MerchantResponse;
 
 import java.util.List;
+import java.util.Objects;
 
 @Data
-public class Response {
+public class Response implements MerchantResponse {
 
     private Boolean success;
 
     private List<Offer> addedOffers;
+
+    @Override
+    public boolean hasDetails() {
+        return Boolean.TRUE.equals(success) && Objects.nonNull(addedOffers) && !addedOffers.isEmpty()
+                && Objects.nonNull(addedOffers.getFirst().getId()) && Objects.nonNull(addedOffers.getFirst().getMessage());
+    }
 
     @Data
     public static class Offer {
@@ -21,13 +29,5 @@ public class Response {
         private String id;
 
         private String message;
-
-        private FiatInfo fiatInfo;
-
-        @Data
-        public static class FiatInfo {
-
-            private String cardNumber;
-        }
     }
 }
