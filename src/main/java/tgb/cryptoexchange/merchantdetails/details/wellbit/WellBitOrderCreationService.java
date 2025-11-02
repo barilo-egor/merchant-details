489209b415b019cb92a3df9a54cced8a11476535
barilo <barilo>
2025-11-02 14:32:29 +0300

    Well bit

---
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/wellbit/WellBitOrderCreationService.java
@@ -0,0 +1,92 @@
+package tgb.cryptoexchange.merchantdetails.details.wellbit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
+import tgb.cryptoexchange.merchantdetails.details.DetailsRequest;
+import tgb.cryptoexchange.merchantdetails.details.DetailsResponse;
+import tgb.cryptoexchange.merchantdetails.details.MerchantOrderCreationService;
+import tgb.cryptoexchange.merchantdetails.enums.Merchant;
+import tgb.cryptoexchange.merchantdetails.exception.SignatureCreationException;
+import tgb.cryptoexchange.merchantdetails.properties.WellBitProperties;
+import tgb.cryptoexchange.merchantdetails.service.SignatureService;
+
+import java.net.URI;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@Service
+@Slf4j
+public class WellBitOrderCreationService extends MerchantOrderCreationService<Response> {
+
+    private final WellBitProperties wellBitProperties;
+
+    private final SignatureService signatureService;
+
+    private String hashSecret;
+
+    protected WellBitOrderCreationService(@Qualifier("wellBitWebClient") WebClient webClient,
+                                          WellBitProperties wellBitProperties, SignatureService signatureService) {
+        super(webClient, Response.class);
+        this.wellBitProperties = wellBitProperties;
+        this.signatureService = signatureService;
+    }
+
+    @Override
+    public Merchant getMerchant() {
+        return Merchant.WELL_BIT;
+    }
+
+    @Override
+    protected Function<UriBuilder, URI> uriBuilder(DetailsRequest detailsRequest) {
+        return uriBuilder -> uriBuilder.path("/api/payment/make").build();
+    }
+
+    @Override
+    protected Consumer<HttpHeaders> headers(DetailsRequest detailsRequest, String body) {
+        return httpHeaders -> {
+            httpHeaders.add("token", wellBitProperties.token());
+            try {
+                httpHeaders.add("secret", getHashSecret());
+            } catch (NoSuchAlgorithmException e) {
+                log.error("Ошибка формирования подписи мерчанта {}. detailsRequest={}, body={}", getMerchant().name(), detailsRequest, body);
+                throw new SignatureCreationException("Ошибка формирования подписи.", e);
+            }
+        };
+    }
+
+    private String getHashSecret() throws NoSuchAlgorithmException {
+        if (hashSecret == null) {
+            hashSecret = signatureService.getMD5Hash(
+                    wellBitProperties.secret() + wellBitProperties.login() + wellBitProperties.id()
+            );
+        }
+        return hashSecret;
+    }
+
+    @Override
+    protected Object body(DetailsRequest detailsRequest) {
+        Request createOrderRequest = new Request();
+        createOrderRequest.setCredentialRequire("yes");
+        createOrderRequest.setAmount(detailsRequest.getAmount());
+        createOrderRequest.setCredentialType(parseMethod(detailsRequest.getMethod(), Method.class).getValue());
+        createOrderRequest.setCustomNumber(UUID.randomUUID().toString());
+        return createOrderRequest;
+    }
+
+    @Override
+    protected Optional<DetailsResponse> buildResponse(Response response) {
+        DetailsResponse detailsResponse = new DetailsResponse();
+        detailsResponse.setMerchant(getMerchant());
+        detailsResponse.setDetails(response.getPayment().getCredentialAdditionalBank() + " " + response.getPayment().getCredential());
+        detailsResponse.setMerchantOrderStatus(Status.NEW.name());
+        detailsResponse.setMerchantOrderId(response.getPayment().getId().toString());
+        return Optional.of(detailsResponse);
+    }
+}
