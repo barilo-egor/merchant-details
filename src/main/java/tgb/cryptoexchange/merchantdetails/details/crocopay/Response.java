1ca5d637f669b3bfd39d225a5dfd8138e4ba94c3
barilo <barilo>
2025-11-04 23:30:19 +0300

    MerchantDetailsResponse croco pay

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/crocopay/Response.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/crocopay/Response.java
@@ -1,13 +1,48 @@
 package tgb.cryptoexchange.merchantdetails.details.crocopay;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import tgb.cryptoexchange.merchantdetails.details.MerchantDetailsResponse;
+import tgb.cryptoexchange.merchantdetails.details.ValidationResult;
+
+import java.util.Objects;
 
 @Data
-public class Response {
+public class Response implements MerchantDetailsResponse {
 
     @JsonProperty("response")
     private ResponseData responseData;
+
+    @Override
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult();
+        if (Objects.isNull(responseData)) {
+            result.notNull("responseData");
+        } else {
+            if (Objects.nonNull(responseData.getTransaction())) {
+                if (Objects.isNull(responseData.getTransaction().getId())) {
+                    result.notNull("responseData.transaction.id");
+                }
+                if (Objects.isNull(responseData.getTransaction().getStatus())) {
+                    result.notNull("responseData.transaction.status");
+                }
+            }
+            if (Objects.nonNull(responseData.getPaymentRequisites())
+                    && Objects.isNull(responseData.getPaymentRequisites().getPaymentMethod())) {
+                result.notNull("responseData.paymentRequisites.paymentMethod");
+            }
+
+        }
+        return result;
+    }
+
+    @Override
+    public boolean hasDetails() {
+        return Objects.nonNull(responseData.getTransaction())
+                && Objects.nonNull(responseData.getPaymentRequisites())
+                && Objects.nonNull(responseData.getPaymentRequisites().getRequisites());
+    }
 
     @Data
     public static class ResponseData {
@@ -22,6 +57,15 @@ public class Response {
             private String paymentMethod;
 
             private String requisites;
+        }
+
+        @Data
+        public static class Transaction {
+
+            private String id;
+
+            @JsonDeserialize(using = Status.Deserializer.class)
+            private Status status;
         }
     }
 }
