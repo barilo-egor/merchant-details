16e3c1a72db3060d52cd9072fae0415223e9159e
barilo <barilo>
2025-11-04 23:47:15 +0300

    MerchantDetailsResponse eze pay

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/ezepay/Response.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/ezepay/Response.java
@@ -2,17 +2,47 @@ package tgb.cryptoexchange.merchantdetails.details.ezepay;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import tgb.cryptoexchange.merchantdetails.details.MerchantDetailsResponse;
+import tgb.cryptoexchange.merchantdetails.details.ValidationResult;
 
 import java.util.Objects;
 
 @Data
-public class Response {
+public class Response implements MerchantDetailsResponse {
 
     private Data data;
 
     private String status;
 
     private String message;
+
+    @Override
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult();
+        if (!"success".equals(status)) {
+            result.addError("status", "expected success, got " + status + " with message \"" + message + "\"");
+        } else {
+            if (Objects.isNull(data)) {
+                result.notNull("data");
+            } else {
+                if (Objects.isNull(data.getOrderId())) {
+                    result.notNull("data.orderId");
+                }
+                if (Objects.isNull(data.getBank()) && Objects.isNull(data.getBankSbp())) {
+                    result.notNull("data.bank", "data.bankSbp");
+                }
+                if (Objects.isNull(data.getDetails())) {
+                    result.notNull("data.details");
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean hasDetails() {
+        return true;
+    }
 
     @lombok.Data
     public static class Data {
