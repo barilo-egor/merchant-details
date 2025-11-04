7bae90a52d1248c971d1a0b90e6719a6490959ca
barilo <barilo>
2025-11-04 23:35:20 +0300

    MerchantDetailsResponse dao

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/daopayments/Response.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/daopayments/Response.java
@@ -3,9 +3,13 @@ package tgb.cryptoexchange.merchantdetails.details.daopayments;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import tgb.cryptoexchange.merchantdetails.details.MerchantDetailsResponse;
+import tgb.cryptoexchange.merchantdetails.details.ValidationResult;
+
+import java.util.Objects;
 
 @Data
-public class Response {
+public class Response implements MerchantDetailsResponse {
 
     @JsonProperty("transaction_id")
     private String transactionId;
@@ -17,6 +21,32 @@ public class Response {
     private TransferDetails transferDetails;
 
     private String amount;
+
+    @Override
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult();
+        if (Objects.isNull(transactionId)) {
+            result.notNull("transactionId");
+        }
+        if (Objects.isNull(status)) {
+            result.notNull("status");
+        }
+        if (Objects.nonNull(transferDetails)) {
+            if (Objects.isNull(transferDetails.getBankName())) {
+                result.notNull("transferDetails.bankName");
+            }
+            if (Objects.isNull(transferDetails.getCardNumber())) {
+                result.notNull("transferDetails.cardNumber");
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean hasDetails() {
+        return Objects.nonNull(transferDetails) && Objects.nonNull(transferDetails.getCardNumber())
+                && Objects.nonNull(transferDetails.getBankName());
+    }
 
     @Data
     public static class TransferDetails {
