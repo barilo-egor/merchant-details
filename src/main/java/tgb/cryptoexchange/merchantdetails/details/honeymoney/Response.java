48229b3656a32093b1cf1607a0a8cf362c9eae8c
barilo <barilo>
2025-11-04 23:51:44 +0300

    MerchantDetailsResponse honeymoney

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/honeymoney/Response.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/honeymoney/Response.java
@@ -1,9 +1,13 @@
 package tgb.cryptoexchange.merchantdetails.details.honeymoney;
 
 import lombok.Data;
+import tgb.cryptoexchange.merchantdetails.details.MerchantDetailsResponse;
+import tgb.cryptoexchange.merchantdetails.details.ValidationResult;
+
+import java.util.Objects;
 
 @Data
-public class Response {
+public class Response implements MerchantDetailsResponse {
 
     private Integer id;
 
@@ -12,4 +16,24 @@ public class Response {
     private String bankName;
 
     private String phoneNumber;
+
+    @Override
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult();
+        if (Objects.isNull(id)) {
+            result.notNull("id");
+        }
+        if (Objects.isNull(cardNumber) && Objects.isNull(phoneNumber)) {
+            result.notNull("cardNumber", "phoneNumber");
+        }
+        if (Objects.isNull(bankName)) {
+            result.notNull("bankName");
+        }
+        return result;
+    }
+
+    @Override
+    public boolean hasDetails() {
+        return true;
+    }
 }
