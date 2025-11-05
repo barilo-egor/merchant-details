6d6f7c9f0531be35c8efbeff689d6b0fb9fe3f2a
barilo <e.barilo@isida.by>
2025-11-05 12:52:43 +0300

    MerchantDetailsResponse onlypays

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/onlypays/Response.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/onlypays/Response.java
@@ -1,17 +1,45 @@
 package tgb.cryptoexchange.merchantdetails.details.onlypays;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import tgb.cryptoexchange.merchantdetails.details.MerchantDetailsResponse;
+import tgb.cryptoexchange.merchantdetails.details.ValidationResult;
+
+import java.util.Objects;
 
 @Data
-public class Response {
+public class Response implements MerchantDetailsResponse {
 
     private boolean success;
 
     private String error;
 
     private Data data;
+
+    @Override
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult();
+        if (!success) {
+            result.addError("success", "expected true, but was false: " + error);
+        } else {
+            if (Objects.nonNull(data)) {
+                if (Objects.isNull(data.getId())) {
+                    result.notNull("data.id");
+                }
+                if (Objects.isNull(data.getRequisite())) {
+                    result.notNull("data.requisite");
+                }
+                if (Objects.isNull(data.getBank())) {
+                    result.notNull("data.bank");
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean hasDetails() {
+        return Objects.nonNull(data);
+    }
 
     @lombok.Data
     public static class Data {
@@ -20,12 +48,6 @@ public class Response {
 
         private String requisite;
 
-        private String owner;
-
         private String bank;
-
-        @JsonProperty("payment_type")
-        @JsonDeserialize(using = Method.Deserializer.class)
-        private Method method;
     }
 }
