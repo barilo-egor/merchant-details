74142ca97796be7d0b9d556fa44e22725d936156
barilo <e.barilo@isida.by>
2025-11-05 15:28:22 +0300

    MerchantDetailsResponse wellbit

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/wellbit/Response.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/wellbit/Response.java
@@ -4,12 +4,41 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import tgb.cryptoexchange.merchantdetails.details.MerchantDetailsResponse;
+import tgb.cryptoexchange.merchantdetails.details.ValidationResult;
+
+import java.util.Objects;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Response {
+public class Response implements MerchantDetailsResponse {
 
     private Payment payment;
+
+    @Override
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult();
+        if (Objects.nonNull(payment)) {
+            if (Objects.isNull(payment.getId())) {
+                result.notNull("payment.id");
+            }
+            if (Objects.isNull(payment.getCredential())) {
+                result.notNull("payment.credential");
+            }
+            if (Objects.isNull(payment.getCredentialAdditionalBank())) {
+                result.notNull("payment.credentialAdditionalBank");
+            }
+            if (Objects.isNull(payment.getStatus())) {
+                result.notNull("payment.status");
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean hasDetails() {
+        return Objects.nonNull(payment);
+    }
 
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
