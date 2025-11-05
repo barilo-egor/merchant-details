65b2d661a81424340f7dc7f23eb93d0d7df5ed19
barilo <e.barilo@isida.by>
2025-11-05 14:30:01 +0300

    MerchantDetailsResponse pspware

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/pspware/Response.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/pspware/Response.java
@@ -2,9 +2,13 @@ package tgb.cryptoexchange.merchantdetails.details.pspware;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import tgb.cryptoexchange.merchantdetails.details.MerchantDetailsResponse;
+import tgb.cryptoexchange.merchantdetails.details.ValidationResult;
+
+import java.util.Objects;
 
 @Data
-public class Response {
+public class Response implements MerchantDetailsResponse {
 
     private String id;
 
@@ -14,4 +18,27 @@ public class Response {
     private String bankName;
 
     private String card;
+
+    @Override
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult();
+        if (Objects.isNull(id)) {
+            result.notNull("id");
+        }
+        if (Objects.isNull(status)) {
+            result.notNull("status");
+        }
+        if (Objects.isNull(bankName)) {
+            result.notNull("bankName");
+        }
+        if (Objects.isNull(card)) {
+            result.notNull("card");
+        }
+        return result;
+    }
+
+    @Override
+    public boolean hasDetails() {
+        return true;
+    }
 }
