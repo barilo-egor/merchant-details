b04499935ea4dcf80a493d63f203874f67c180f1
barilo <barilo>
2025-11-07 20:55:45 +0300

    удаление неиспользуемых мерчантов

--- src/main/java/tgb/cryptoexchange/merchantdetails/properties/PayPointsProperties.java
+++
@@ -1,7 +0,0 @@
-package tgb.cryptoexchange.merchantdetails.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "pay-points")
-public record PayPointsProperties(String url, String token, String signKey) implements PayBoxProperties {
-}
