4cd0077e992e594323c5402ac714bc467f9914d9
barilo <barilo>
2025-11-09 12:34:51 +0300

    ya pay тест

---
+++ src/test/java/tgb/cryptoexchange/merchantdetails/details/paybox/YaPayOrderCreationServiceTest.java
@@ -0,0 +1,21 @@
+package tgb.cryptoexchange.merchantdetails.details.paybox;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tgb.cryptoexchange.merchantdetails.enums.Merchant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class YaPayOrderCreationServiceTest {
+
+    @InjectMocks
+    private YaPayOrderCreationService yaPayOrderCreationService;
+
+    @Test
+    void getMerchantShouldReturnYaPay() {
+        assertEquals(Merchant.YA_PAY, yaPayOrderCreationService.getMerchant());
+    }
+}
