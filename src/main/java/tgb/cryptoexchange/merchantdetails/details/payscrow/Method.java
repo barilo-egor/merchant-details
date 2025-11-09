3f6dc14778ae1339642ad4434956aea3bb043962
barilo <barilo>
2025-11-09 19:49:02 +0300

    новый метод payscrow

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/payscrow/Method.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/payscrow/Method.java
@@ -15,6 +15,7 @@ public enum Method {
     BANK_CARD("8fe3669a-a448-4053-bc4b-43bb51cb3e9d", "Карта"),
     TRANS_SBP("c154be74-5e4e-4edb-85c2-d2aa175165f5", "Трансгран СБП"),
     BEELINE("f671777f-ab51-49af-b322-b0a1647568d2", "Билайн сим карты"),
+    TRIANGLE("53035abd-b34e-4bd7-9305-e5546320a3b9", "Треугольники")
     ;
 
     private final String value;
