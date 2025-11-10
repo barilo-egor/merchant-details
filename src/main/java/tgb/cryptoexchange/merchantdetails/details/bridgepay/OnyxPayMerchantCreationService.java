f95af0ab749fc1de41a18625afbec41ae399053d
barilo <e.barilo@isida.by>
2025-11-10 13:14:30 +0300

    onyx pay sim

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/bridgepay/OnyxPayMerchantCreationService.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/bridgepay/OnyxPayMerchantCreationService.java
@@ -7,12 +7,27 @@ import tgb.cryptoexchange.merchantdetails.enums.Merchant;
 import tgb.cryptoexchange.merchantdetails.properties.OnyxPayProperties;
 import tgb.cryptoexchange.merchantdetails.service.SignatureService;
 
+import java.util.function.Function;
+
 @Service
 public class OnyxPayMerchantCreationService extends BridgePayOrderCreationService {
+
+    private final OnyxPayProperties onyxPayProperties;
 
     protected OnyxPayMerchantCreationService(@Qualifier("onyxPayWebClient") WebClient webClient,
                                              OnyxPayProperties onyxPayProperties, SignatureService signatureService) {
         super(webClient, onyxPayProperties, signatureService);
+        this.onyxPayProperties = onyxPayProperties;
+    }
+
+    @Override
+    protected Function<Method, String> keyFunction() {
+        return method -> {
+            if (Method.MOBILE_TOP_UP.equals(method)) {
+                return onyxPayProperties.simKey();
+            }
+            return onyxPayProperties.key();
+        };
     }
 
     @Override
