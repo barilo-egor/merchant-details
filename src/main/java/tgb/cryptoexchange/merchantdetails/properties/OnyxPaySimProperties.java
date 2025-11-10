dc8b01c5011127a4d8288044ffd8cce64c315a96
barilo <e.barilo@isida.by>
2025-11-10 12:45:14 +0300

    Revert "onyx pay sim"
    This reverts commit db03017efcd812de4322f6efa14c8ce07e954522.

--- src/main/java/tgb/cryptoexchange/merchantdetails/properties/OnyxPaySimProperties.java
+++
@@ -1,7 +0,0 @@
-package tgb.cryptoexchange.merchantdetails.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "onyx-pay-sim")
-public record OnyxPaySimProperties(String url, String token, String key, String secret) implements BridgePayProperties {
-}
