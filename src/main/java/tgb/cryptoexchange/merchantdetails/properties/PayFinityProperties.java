e1415d40432afcb8d6c6827ff5b5b1e82639f3e9
barilo <e.barilo@isida.by>
2025-11-10 11:35:10 +0300

    storm trade props test + удаление пропертей неактивных мерчантов

--- src/main/java/tgb/cryptoexchange/merchantdetails/properties/PayFinityProperties.java
+++
@@ -1,9 +0,0 @@
-package tgb.cryptoexchange.merchantdetails.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "pay-finity")
-public record PayFinityProperties(String url, Keys keys, Integer timeout) {
-    public record Keys(String publicKey, String privateKey) {
-    }
-}
