82c0616fbc6fb30f0c09d97fd91ee960396ad56f
barilo <e.barilo@isida.by>
2025-11-12 17:35:57 +0300

    новые методы, разделение методов по терминалам payscrow проверка валидности запроса реквизитов

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/payscrow/PayscrowOrderCreationServiceImpl.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/payscrow/PayscrowOrderCreationServiceImpl.java
@@ -3,19 +3,59 @@ package tgb.cryptoexchange.merchantdetails.details.payscrow;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import tgb.cryptoexchange.merchantdetails.details.DetailsRequest;
 import tgb.cryptoexchange.merchantdetails.enums.Merchant;
 import tgb.cryptoexchange.merchantdetails.properties.PayscrowPropertiesImpl;
+
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 @Service
 public class PayscrowOrderCreationServiceImpl extends PayscrowOrderCreationService {
 
+    private static final int AMOUNT_BOUND = 10_000;
+
+    private static final  Set<Method> IN_HOUSE_METHODS = Set.of(
+            Method.ALFA, Method.PSB, Method.GAZ_PROM, Method.OZON
+    );
+
+    private final PayscrowPropertiesImpl payscrowProperties;
+
     protected PayscrowOrderCreationServiceImpl(@Qualifier("payscrowWebClient") WebClient webClient,
                                                PayscrowPropertiesImpl payscrowProperties) {
         super(webClient, payscrowProperties);
+        this.payscrowProperties = payscrowProperties;
+    }
+
+    @Override
+    protected Predicate<DetailsRequest> isValidRequestPredicate() {
+        return detailsRequest -> {
+            Method method = parseMethod(detailsRequest.getMethod(), Method.class);
+            return !Method.TRIANGLE.equals(method) || detailsRequest.getAmount() < AMOUNT_BOUND;
+        };
     }
 
     @Override
     public Merchant getMerchant() {
         return Merchant.PAYSCROW;
+    }
+
+    @Override
+    public Function<DetailsRequest, String> keyFunction() {
+        return detailsRequest -> {
+            Method method = parseMethod(detailsRequest.getMethod(), Method.class);
+            if (IN_HOUSE_METHODS.contains(method)) {
+                return payscrowProperties.inHouseKey();
+            }
+            if (Method.TRIANGLE.equals(method)) {
+                return payscrowProperties.whiteTriangleKey();
+            }
+            if (detailsRequest.getAmount() < AMOUNT_BOUND) {
+                return payscrowProperties.key();
+            } else {
+                return payscrowProperties.highCheckKey();
+            }
+        };
     }
 }
--- src/test/java/tgb/cryptoexchange/merchantdetails/details/payscrow/LuckyPayOrderCreationServiceTest.java
+++ src/test/java/tgb/cryptoexchange/merchantdetails/details/payscrow/LuckyPayOrderCreationServiceTest.java
@@ -2,14 +2,23 @@ package tgb.cryptoexchange.merchantdetails.details.payscrow;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tgb.cryptoexchange.merchantdetails.details.DetailsRequest;
 import tgb.cryptoexchange.merchantdetails.enums.Merchant;
+import tgb.cryptoexchange.merchantdetails.properties.LuckyPayProperties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class LuckyPayOrderCreationServiceTest {
+
+    @Mock
+    private LuckyPayProperties luckyPayProperties;
 
     @InjectMocks
     private LuckyPayOrderCreationService service;
@@ -17,5 +26,27 @@ class LuckyPayOrderCreationServiceTest {
     @Test
     void getMerchantShouldReturnLuckyPay() {
         assertEquals(Merchant.LUCKY_PAY, service.getMerchant());
+    }
+
+    @Test
+    void keyFunctionShouldReturnKeyIfDetailsRequestIsNull() {
+        when(luckyPayProperties.key()).thenReturn("key");
+        assertEquals("key", service.keyFunction().apply(null));
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            method1,1000,53355,https://google.com,12515050,someKey1
+            method2,2000,12586,https://example.com,6423525253,some-key-2
+            """)
+    void keyFunctionShouldAlwaysReturnKey(String method, Integer amount, Long id, String callbackUrl, Long chatId, String key) {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setMethod(method);
+        detailsRequest.setAmount(amount);
+        detailsRequest.setId(id);
+        detailsRequest.setCallbackUrl(callbackUrl);
+        detailsRequest.setChatId(chatId);
+        when(luckyPayProperties.key()).thenReturn(key);
+        assertEquals(luckyPayProperties.key(), key);
     }
 }
