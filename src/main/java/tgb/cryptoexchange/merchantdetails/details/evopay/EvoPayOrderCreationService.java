f4e48e3d9ca8a6e7a5ae8fbde2d6cac01c461a33
barilo <e.barilo@isida.by>
2025-11-13 12:37:18 +0300

    evo pay получение реквизитов спустя задержку после создания ордера

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/evopay/EvoPayOrderCreationService.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/evopay/EvoPayOrderCreationService.java
@@ -1,15 +1,21 @@
 package tgb.cryptoexchange.merchantdetails.details.evopay;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriBuilder;
+import tgb.cryptoexchange.exception.ServiceUnavailableException;
 import tgb.cryptoexchange.merchantdetails.details.DetailsRequest;
 import tgb.cryptoexchange.merchantdetails.details.DetailsResponse;
 import tgb.cryptoexchange.merchantdetails.details.MerchantOrderCreationService;
 import tgb.cryptoexchange.merchantdetails.enums.Merchant;
 import tgb.cryptoexchange.merchantdetails.properties.EvoPayProperties;
+import tgb.cryptoexchange.merchantdetails.service.SleepingService;
 
 import java.net.URI;
 import java.util.Objects;
@@ -19,14 +25,73 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 @Service
+@Slf4j
 public class EvoPayOrderCreationService extends MerchantOrderCreationService<Response> {
+
+    private static final String ENTRIES_FIELD = "entries";
 
     private final EvoPayProperties evoPayProperties;
 
+    private final WebClient webClient;
+
+    private final SleepingService sleepingService;
+
     protected EvoPayOrderCreationService(@Qualifier("evoPayWebClient") WebClient webClient,
-                                         EvoPayProperties evoPayProperties) {
+                                         EvoPayProperties evoPayProperties, SleepingService sleepingService) {
         super(webClient, Response.class);
         this.evoPayProperties = evoPayProperties;
+        this.webClient = webClient;
+        this.sleepingService = sleepingService;
+    }
+
+    @Override
+    protected Optional<String> makeRequest(DetailsRequest detailsRequest, String body) {
+        Optional<String> createOrderResponse = super.makeRequest(detailsRequest, body);
+        if (createOrderResponse.isEmpty()) {
+            log.debug("Отсутствует тело ответа при создании ордера мерчанта {}.", getMerchant().name());
+            return Optional.empty();
+        }
+        String createOrderResponseBody = createOrderResponse.get();
+        Response response;
+        try {
+             response = objectMapper.readValue(createOrderResponseBody, Response.class);
+        } catch (JsonProcessingException e) {
+            long currentTime = System.currentTimeMillis();
+            log.debug("{} Ошибки преобразования ответа при создании ордера мерчанта {}, body: {}",
+                    currentTime, getMerchant().name(), createOrderResponseBody);
+            throw new ServiceUnavailableException("Error occurred while mapping create order response: " + currentTime);
+        }
+        try {
+            sleepingService.sleep(8);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            long currentTime = System.currentTimeMillis();
+            log.error("{} Ошибка при ожидании: {}", currentTime, e.getMessage(), e);
+            throw new ServiceUnavailableException("Error occurred while wait: " + currentTime);
+        }
+        String listOrderStringResponse = requestService.request(
+                webClient,
+                HttpMethod.GET,
+                uriBuilder -> uriBuilder.path("/v1/api/order/list").queryParam("order_id", response.getId()).build(),
+                this.headers(detailsRequest, body),
+                body
+        );
+        JsonNode listOrderResponse;
+        try {
+            listOrderResponse = objectMapper.readTree(listOrderStringResponse);
+        } catch (JsonProcessingException e) {
+            long currentTime = System.currentTimeMillis();
+            log.debug("{} Ошибки преобразования ответа списка ордеров мерчанта {}, body: {}", currentTime,
+                    getMerchant().name(), createOrderResponseBody);
+            throw new ServiceUnavailableException("Error occurred while mapping get order response: " + currentTime);
+        }
+        if (listOrderResponse.has(ENTRIES_FIELD)) {
+            JsonNode entries = listOrderResponse.get(ENTRIES_FIELD);
+            if (entries.isArray() && !entries.isEmpty()) {
+                return Optional.of(listOrderResponse.get(ENTRIES_FIELD).get(0).toPrettyString());
+            }
+        }
+        return Optional.empty();
     }
 
     @Override
--- src/test/java/tgb/cryptoexchange/merchantdetails/details/evopay/EvoPayOrderCreationServiceTest.java
+++ src/test/java/tgb/cryptoexchange/merchantdetails/details/evopay/EvoPayOrderCreationServiceTest.java
@@ -1,35 +1,64 @@
 package tgb.cryptoexchange.merchantdetails.details.evopay;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
+import tgb.cryptoexchange.exception.ServiceUnavailableException;
 import tgb.cryptoexchange.merchantdetails.details.DetailsRequest;
 import tgb.cryptoexchange.merchantdetails.details.DetailsResponse;
 import tgb.cryptoexchange.merchantdetails.enums.Merchant;
 import tgb.cryptoexchange.merchantdetails.properties.EvoPayProperties;
+import tgb.cryptoexchange.merchantdetails.service.RequestService;
+import tgb.cryptoexchange.merchantdetails.service.SleepingService;
 
+import java.net.URI;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class EvoPayOrderCreationServiceTest {
 
     @Mock
+    private RequestService requestService;
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @Mock
     private EvoPayProperties evoPayProperties;
+
+    @Mock
+    private SleepingService sleepingService;
 
     @InjectMocks
     private EvoPayOrderCreationService evoPayOrderCreationService;
+
+    @BeforeEach
+    void setUp() {
+        evoPayOrderCreationService.setRequestService(requestService);
+        evoPayOrderCreationService.setObjectMapper(objectMapper);
+    }
 
     @Test
     void getMerchantShouldReturnEvoPayMerchant() {
@@ -143,5 +172,146 @@ class EvoPayOrderCreationServiceTest {
                 () -> assertEquals(status.name(), actual.getMerchantOrderStatus()),
                 () -> assertEquals(bank + " " + requisiteString, actual.getDetails())
         );
+    }
+
+    @Test
+    void makeRequestShouldReturnEmptyOptionalIfNoResponse() {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setAmount(2000);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn(null);
+        assertTrue(evoPayOrderCreationService.makeRequest(detailsRequest, "").isEmpty());
+    }
+
+    @Test
+    void makeRequestShouldThrowServiceUnavailableIfJsonProcessingExceptionWasThrownWhileMappingCreateOrderResponse() throws JsonProcessingException {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setAmount(2000);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        when(objectMapper.readValue(anyString(), eq(Response.class))).thenThrow(JsonProcessingException.class);
+        assertThrows(ServiceUnavailableException.class, () -> evoPayOrderCreationService.makeRequest(detailsRequest, ""));
+    }
+
+    @Test
+    void makeRequestShouldThrowServiceUnavailableIfInterruptedExceptionWasThrown() throws JsonProcessingException, InterruptedException {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setAmount(2000);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        Response response = new Response();
+        when(objectMapper.readValue(anyString(), eq(Response.class))).thenReturn(response);
+        doThrow(InterruptedException.class).when(sleepingService).sleep(8);
+        assertThrows(ServiceUnavailableException.class, () -> evoPayOrderCreationService.makeRequest(detailsRequest, ""));
+    }
+
+    @ValueSource(strings = {
+            "cbf6c676-1adb-4f03-81b0-67c26dea863c",
+            "8a7e1e35-6ced-41ec-9949-acbdd52076c7"
+    })
+    @ParameterizedTest
+    void makeRequestShouldMakeRequestToListOrdersUrlWithOrderIdParam(String id) throws JsonProcessingException {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setAmount(2000);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        Response response = new Response();
+        response.setId(id);
+        when(objectMapper.readValue(anyString(), eq(Response.class))).thenReturn(response);
+        @SuppressWarnings("unchecked")
+        ArgumentCaptor<Function<UriBuilder, URI>> captor =
+                (ArgumentCaptor<Function<UriBuilder, URI>>) (ArgumentCaptor<?>) ArgumentCaptor.forClass(Function.class);
+        when(objectMapper.readTree(anyString())).thenThrow(JsonProcessingException.class);
+        assertThrows(ServiceUnavailableException.class, () -> evoPayOrderCreationService.makeRequest(detailsRequest, ""));
+        verify(requestService).request(any(), eq(HttpMethod.GET), captor.capture(), any(), anyString());
+        UriBuilder uriBuilder = UriComponentsBuilder.newInstance();
+        URI actual = captor.getValue().apply(uriBuilder);
+        assertTrue(actual.toString().endsWith("/v1/api/order/list?order_id=" + id));
+    }
+
+    @Test
+    void makeRequestShouldThrowServiceUnavailableIfJsonProcessingExceptionWasThrownWhileMappingGetOrderResponse() throws JsonProcessingException {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setAmount(2000);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        Response response = new Response();
+        when(objectMapper.readValue(anyString(), eq(Response.class))).thenReturn(response);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        when(objectMapper.readTree(anyString())).thenThrow(JsonProcessingException.class);
+        assertThrows(ServiceUnavailableException.class, () -> evoPayOrderCreationService.makeRequest(detailsRequest, ""));
+    }
+
+    @Test
+    void makeRequestShouldReturnEmptyOptionalIfHasNoEntries() throws JsonProcessingException {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setAmount(2000);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        Response response = new Response();
+        when(objectMapper.readValue(anyString(), eq(Response.class))).thenReturn(response);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        JsonNode responseNode = Mockito.mock(JsonNode.class);
+        when(objectMapper.readTree(anyString())).thenReturn(responseNode);
+        when(responseNode.has("entries")).thenReturn(false);
+        assertTrue(evoPayOrderCreationService.makeRequest(detailsRequest, "").isEmpty());
+    }
+
+    @Test
+    void makeRequestShouldReturnEmptyOptionalIfEntriesIsNotArray() throws JsonProcessingException {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setAmount(2000);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        Response response = new Response();
+        when(objectMapper.readValue(anyString(), eq(Response.class))).thenReturn(response);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        JsonNode responseNode = Mockito.mock(JsonNode.class);
+        when(objectMapper.readTree(anyString())).thenReturn(responseNode);
+        when(responseNode.has("entries")).thenReturn(true);
+        JsonNode entries = Mockito.mock(JsonNode.class);
+        when(responseNode.get("entries")).thenReturn(entries);
+        when(entries.isArray()).thenReturn(false);
+        assertTrue(evoPayOrderCreationService.makeRequest(detailsRequest, "").isEmpty());
+    }
+
+    @Test
+    void makeRequestShouldReturnEmptyOptionalIfEntriesIsEmpty() throws JsonProcessingException {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setAmount(2000);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        Response response = new Response();
+        when(objectMapper.readValue(anyString(), eq(Response.class))).thenReturn(response);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        JsonNode responseNode = Mockito.mock(JsonNode.class);
+        when(objectMapper.readTree(anyString())).thenReturn(responseNode);
+        when(responseNode.has("entries")).thenReturn(true);
+        JsonNode entries = Mockito.mock(JsonNode.class);
+        when(responseNode.get("entries")).thenReturn(entries);
+        when(entries.isArray()).thenReturn(true);
+        when(entries.isEmpty()).thenReturn(true);
+        assertTrue(evoPayOrderCreationService.makeRequest(detailsRequest, "").isEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "{\"id\":\"550e8400-e29b-41d4-a716-446655440000\",\"orderStatus\":\"SUCCESS\",\"requisites\":" +
+                    "{\"recipient_phone_number\":\"+79991234567\",\"recipient_card_number\":null,\"recipient_bank\":\"Sberbank\"}}",
+            "{\"id\":\"123e4567-e89b-12d3-a456-426614174000\",\"orderStatus\":\"PENDING\",\"requisites\":" +
+                    "{\"recipient_phone_number\":null,\"recipient_card_number\":\"4111111111111111\",\"recipient_bank\":\"Tinkoff\"}}"
+    })
+    void makeRequestShouldReturnEmptyOptionalIfEntriesIsEmpty(String orderBody) throws JsonProcessingException {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setAmount(2000);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        Response response = new Response();
+        when(objectMapper.readValue(anyString(), eq(Response.class))).thenReturn(response);
+        when(requestService.request(any(), any(), any(), any(), anyString())).thenReturn("");
+        JsonNode responseNode = Mockito.mock(JsonNode.class);
+        when(objectMapper.readTree(anyString())).thenReturn(responseNode);
+        when(responseNode.has("entries")).thenReturn(true);
+        JsonNode entries = Mockito.mock(JsonNode.class);
+        when(responseNode.get("entries")).thenReturn(entries);
+        when(entries.isArray()).thenReturn(true);
+        when(entries.isEmpty()).thenReturn(false);
+        JsonNode order = Mockito.mock(JsonNode.class);
+        when(entries.get(0)).thenReturn(order);
+        when(order.toPrettyString()).thenReturn(orderBody);
+        Optional<String> maybeResponse = evoPayOrderCreationService.makeRequest(detailsRequest, "");
+        assertTrue(maybeResponse.isPresent());
+        assertEquals(orderBody, maybeResponse.get());
     }
 }
