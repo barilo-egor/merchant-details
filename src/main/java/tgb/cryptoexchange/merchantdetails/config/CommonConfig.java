a17e51dacc319fb1ae2d4655e712da56c4f0719a
barilo <e.barilo@isida.by>
2025-11-14 15:23:21 +0300

    получение сервера для кафки из конфига

--- src/main/java/tgb/cryptoexchange/merchantdetails/config/CommonConfig.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/config/CommonConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -18,6 +19,12 @@ import java.util.Map;
 @Configuration
 public class CommonConfig {
 
+    private final KafkaProperties kafkaProperties;
+
+    public CommonConfig(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -28,6 +35,7 @@ public class CommonConfig {
     @Bean
     public ProducerFactory<String, MerchantDetailsReceiveEvent> merchantHistoryProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, MerchantDetailsReceiveEventSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
