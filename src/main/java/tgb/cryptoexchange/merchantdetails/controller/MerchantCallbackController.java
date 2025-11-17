b8a4e4cfd2d374231164dbe649f66564e00944c9
barilo <e.barilo@isida.by>
2025-11-17 15:45:18 +0300

    добавлена проверка secret

--- src/main/java/tgb/cryptoexchange/merchantdetails/controller/MerchantCallbackController.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/controller/MerchantCallbackController.java
@@ -1,6 +1,8 @@
 package tgb.cryptoexchange.merchantdetails.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tgb.cryptoexchange.controller.ApiController;
 import tgb.cryptoexchange.merchantdetails.enums.Merchant;
@@ -12,13 +14,19 @@ public class MerchantCallbackController extends ApiController {
 
     private final MerchantDetailsService merchantDetailsService;
 
-    public MerchantCallbackController(MerchantDetailsService merchantDetailsService) {
+    private final String secret;
+
+    public MerchantCallbackController(MerchantDetailsService merchantDetailsService, @Value("${callback-secret}") String secret) {
         this.merchantDetailsService = merchantDetailsService;
+        this.secret = secret;
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @PostMapping
-    public void callback(@RequestParam Merchant merchant, @RequestBody String callbackBody) {
+    public ResponseEntity<Void> callback(@RequestParam Merchant merchant, @RequestParam String secret, @RequestBody String callbackBody) {
+        if (this.secret.equals(secret)) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         merchantDetailsService.updateStatus(merchant, callbackBody);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
