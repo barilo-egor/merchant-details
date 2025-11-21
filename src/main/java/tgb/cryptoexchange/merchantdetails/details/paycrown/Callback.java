f1fd9d21952d386fada5cd2d38f4e27519b1e0e3
barilo <e.barilo@isida.by>
2025-11-21 17:47:16 +0300

    обработка callback PayCrown

---
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/paycrown/Callback.java
@@ -0,0 +1,40 @@
+package tgb.cryptoexchange.merchantdetails.details.paycrown;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.Data;
+import tgb.cryptoexchange.merchantdetails.details.MerchantCallback;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Data
+public class Callback implements MerchantCallback {
+
+    private String id;
+
+    @JsonDeserialize(using = Status.Deserializer.class)
+    @JsonProperty("status")
+    private Status responseStatus;
+
+    @Override
+    public Optional<String> getMerchantOrderId() {
+        return Optional.ofNullable(id);
+    }
+
+    @Override
+    public Optional<String> getStatus() {
+        if (Objects.isNull(responseStatus)) {
+            return Optional.empty();
+        }
+        return Optional.of(responseStatus.name());
+    }
+
+    @Override
+    public Optional<String> getStatusDescription() {
+        if (Objects.isNull(responseStatus)) {
+            return Optional.empty();
+        }
+        return Optional.of(responseStatus.getDescription());
+    }
+}
