a70f78abbb8ac081f6a7a7575e026ef88ae2b21c
barilo <e.barilo@isida.by>
2025-11-24 15:36:50 +0300

    исправлен конфликт для джексона

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/paybox/Callback.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/paybox/Callback.java
@@ -1,25 +1,16 @@
 package tgb.cryptoexchange.merchantdetails.details.paybox;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import tgb.cryptoexchange.merchantdetails.details.UnwrappedCallback;
 
-import java.util.Objects;
-
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class Callback extends UnwrappedCallback {
 
-    @JsonProperty("id")
-    private Long responseId;
+    private String id;
 
     @JsonDeserialize(using = Status.Deserializer.class)
     private Status status;
-
-    @Override
-    public String getId() {
-        return Objects.nonNull(responseId) ? responseId.toString() : null;
-    }
 }
