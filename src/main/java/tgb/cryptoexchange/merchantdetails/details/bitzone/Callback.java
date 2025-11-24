aea2f1586ace5e259a7ef5c2e207e9e09abf0dfd
barilo <e.barilo@isida.by>
2025-11-24 17:03:57 +0300

    BitZone обработка callback

---
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/bitzone/Callback.java
@@ -0,0 +1,16 @@
+package tgb.cryptoexchange.merchantdetails.details.bitzone;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import tgb.cryptoexchange.merchantdetails.details.UnwrappedCallback;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class Callback extends UnwrappedCallback {
+
+    private String id;
+
+    @JsonDeserialize(using = Status.Deserializer.class)
+    private Status status;
+}
