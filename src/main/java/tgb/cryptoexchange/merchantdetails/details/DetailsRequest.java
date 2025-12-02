218f180b18a7d0a840144efd2ffd9c674c4f870b
barilo <e.barilo@isida.by>
2025-12-02 12:11:49 +0300

    перенос конфигурации мерчантов и метода получения реквизитов среди всех мерчантов

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/DetailsRequest.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/DetailsRequest.java
@@ -1,25 +1,15 @@
 package tgb.cryptoexchange.merchantdetails.details;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class DetailsRequest {
-
-    private Long id;
+public class DetailsRequest extends CommonDetailsRequest {
 
     @NotBlank
     private String method;
-
-    @NotNull
-    @Min(1)
-    private Integer amount;
-
-    private Long chatId;
-
-    private String initiatorApp;
 }
---
+++ src/main/java/tgb/cryptoexchange/merchantdetails/repository/MerchantConfigRepository.java
@@ -0,0 +1,23 @@
+package tgb.cryptoexchange.merchantdetails.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import tgb.cryptoexchange.merchantdetails.entity.MerchantConfig;
+
+import java.util.List;
+
+@Repository
+public interface MerchantConfigRepository extends JpaRepository<MerchantConfig, Long> {
+
+    List<MerchantConfig> findAllByOrderByMerchantOrder();
+
+    List<MerchantConfig> findAllByIsOnOrderByMerchantOrder(Boolean isOn);
+
+    @Query("select max(merchantOrder) from MerchantConfig")
+    Integer finMaxMerchantOrder();
+
+    MerchantConfig findByMerchantOrder(Integer merchantOrder);
+
+    List<MerchantConfig> findAllByIsOn(boolean isOn);
+}
