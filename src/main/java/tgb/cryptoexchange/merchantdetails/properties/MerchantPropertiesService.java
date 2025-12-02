a0141d3d7b5c9f6d111d948c8a2d3402b7aa688e
barilo <e.barilo@isida.by>
2025-12-02 10:46:16 +0300

    удалены все проперти из MerchantPropertiesService кроме мерчанта EvoPay

--- src/main/java/tgb/cryptoexchange/merchantdetails/properties/MerchantPropertiesService.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/properties/MerchantPropertiesService.java
@@ -7,7 +7,7 @@ import java.util.EnumMap;
 import java.util.Map;
 import java.util.Optional;
 
-import static tgb.cryptoexchange.merchantdetails.enums.Merchant.*;
+import static tgb.cryptoexchange.merchantdetails.enums.Merchant.EVO_PAY;
 
 /**
  * Класс создан на время трансфера сервиса из монолита
@@ -17,45 +17,9 @@ public class MerchantPropertiesService {
 
     private final Map<Merchant, Object> properties;
 
-    public MerchantPropertiesService(AlfaTeamProperties alfaTeamProperties,
-                                     BitZoneProperties bitZoneProperties,
-                                     CrocoPayProperties crocoPayProperties,
-                                     EvoPayProperties evoPayProperties,
-                                     ExtasyPayProperties extasyPayProperties,
-                                     FoxPaysProperties foxPaysProperties,
-                                     GeoTransferProperties geoTransferProperties,
-                                     LuckyPayProperties luckyPayProperties,
-                                     MobiusProperties mobiusProperties,
-                                     NicePayProperties nicePayProperties,
-                                     OnlyPaysProperties onlyPaysProperties,
-                                     OnyxPayProperties onyxPayProperties,
-                                     YaPayProperties yaPayProperties,
-                                     PayLeeProperties payLeeProperties,
-                                     PayscrowPropertiesImpl payscrowPropertiesImpl,
-                                     PspWareProperties pspWareProperties,
-                                     RostrastProperties rostrastProperties,
-                                     WellBitProperties wellBitProperties,
-                                     StormTradeProperties stormTradeProperties) {
+    public MerchantPropertiesService(EvoPayProperties evoPayProperties) {
         properties = new EnumMap<>(Merchant.class);
-        properties.put(ALFA_TEAM, alfaTeamProperties);
-        properties.put(BIT_ZONE, bitZoneProperties);
-        properties.put(CROCO_PAY, crocoPayProperties);
         properties.put(EVO_PAY, evoPayProperties);
-        properties.put(EXTASY_PAY, extasyPayProperties);
-        properties.put(FOX_PAYS, foxPaysProperties);
-        properties.put(GEO_TRANSFER, geoTransferProperties);
-        properties.put(LUCKY_PAY, luckyPayProperties);
-        properties.put(MOBIUS, mobiusProperties);
-        properties.put(NICE_PAY, nicePayProperties);
-        properties.put(ONLY_PAYS, onlyPaysProperties);
-        properties.put(ONYX_PAY, onyxPayProperties);
-        properties.put(YA_PAY, yaPayProperties);
-        properties.put(PAY_LEE, payLeeProperties);
-        properties.put(PAYSCROW, payscrowPropertiesImpl);
-        properties.put(PSP_WARE, pspWareProperties);
-        properties.put(ROSTRAST, rostrastProperties);
-        properties.put(WELL_BIT, wellBitProperties);
-        properties.put(STORM_TRADE, stormTradeProperties);
     }
 
     public Optional<Object> getProperties(Merchant merchant) {
