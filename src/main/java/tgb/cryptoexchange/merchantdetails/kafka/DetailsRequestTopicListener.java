7456a4913adc9c7176955380ea80b402dc81ebd2
barilo <barilo>
2025-12-14 22:06:32 +0300

    распределение запросов по потокам

--- src/main/java/tgb/cryptoexchange/merchantdetails/kafka/DetailsRequestTopicListener.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/kafka/DetailsRequestTopicListener.java
@@ -1,39 +1,22 @@
 package tgb.cryptoexchange.merchantdetails.kafka;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import tgb.cryptoexchange.merchantdetails.details.DetailsRequest;
-import tgb.cryptoexchange.merchantdetails.details.DetailsResponse;
-import tgb.cryptoexchange.merchantdetails.service.MerchantDetailsService;
-
-import java.util.Optional;
 
 @Service
 @Profile("!kafka-disabled")
 public class DetailsRequestTopicListener {
 
-    private final MerchantDetailsService merchantDetailsService;
+    private final DetailsRequestProcessorService detailsRequestProcessorService;
 
-    private final KafkaTemplate<String, DetailsResponse> detailsResponseKafkaTemplate;
-
-    private final String detailsResponseFoundTopic;
-
-    public DetailsRequestTopicListener(MerchantDetailsService merchantDetailsService, KafkaTemplate<String, DetailsResponse> detailsResponseKafkaTemplate,
-                                       @Value("${kafka.topic.merchant-details.response}") String detailsResponseFoundTopic) {
-        this.merchantDetailsService = merchantDetailsService;
-        this.detailsResponseKafkaTemplate = detailsResponseKafkaTemplate;
-        this.detailsResponseFoundTopic = detailsResponseFoundTopic;
+    public DetailsRequestTopicListener(DetailsRequestProcessorService detailsRequestProcessorService) {
+        this.detailsRequestProcessorService = detailsRequestProcessorService;
     }
 
     @KafkaListener(topics = "${kafka.topic.merchant-details.request}", groupId = "${kafka.group-id}")
     public void receive(DetailsRequest request) {
-        Optional<DetailsResponse> detailsResponse = merchantDetailsService.getDetails(request);
-        DetailsResponse result;
-        result = detailsResponse.orElseGet(DetailsResponse::new);
-        result.setRequestId(request.getRequestId());
-        detailsResponseKafkaTemplate.send(detailsResponseFoundTopic, result.getRequestId(), result);
+        detailsRequestProcessorService.process(request);
     }
 }
--- src/test/java/tgb/cryptoexchange/merchantdetails/kafka/DetailsRequestTopicListenerTest.java
+++ src/test/java/tgb/cryptoexchange/merchantdetails/kafka/DetailsRequestTopicListenerTest.java
@@ -1,82 +1,30 @@
 package tgb.cryptoexchange.merchantdetails.kafka;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.kafka.core.KafkaTemplate;
-import tgb.cryptoexchange.merchantdetails.constants.Merchant;
 import tgb.cryptoexchange.merchantdetails.details.DetailsRequest;
-import tgb.cryptoexchange.merchantdetails.details.DetailsResponse;
-import tgb.cryptoexchange.merchantdetails.service.MerchantDetailsService;
 
-import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DetailsRequestTopicListenerTest {
 
     @Mock
-    private MerchantDetailsService merchantDetailsService;
+    private DetailsRequestProcessorService detailsRequestProcessorService;
 
-    @Mock
-    private KafkaTemplate<String, DetailsResponse> kafkaTemplate;
-
-    private DetailsRequestTopicListener listener;
-
-    @BeforeEach
-    void setUp() {
-        listener = new DetailsRequestTopicListener(merchantDetailsService, kafkaTemplate,
-                "merchant-details-request-topic");
-    }
+    @InjectMocks
+    private DetailsRequestTopicListener detailsRequestTopicListener;
 
     @Test
-    void receiveShouldSendEmptyResponseIfDetailsIsEmpty() {
-        DetailsRequest request = new DetailsRequest();
-        String id = UUID.randomUUID().toString();
-        request.setRequestId(id);
-        when(merchantDetailsService.getDetails(request)).thenReturn(Optional.empty());
-        listener.receive(request);
-        ArgumentCaptor<DetailsResponse> captor = ArgumentCaptor.forClass(DetailsResponse.class);
-        verify(kafkaTemplate).send(eq("merchant-details-request-topic"), eq(id), captor.capture());
-        assertEquals(id, captor.getValue().getRequestId());
-    }
-
-    @CsvSource("""
-            ALFA 1234 1234 1234 1234,ALFA_TEAM,PENDING,5000
-            Ozon bank 79879878787,ONLY_PAYS,PROCESS,6432
-            """)
-    @ParameterizedTest
-    void receiveShouldSendDetailsObjectIfDetailsFound(String details, Merchant merchant, String status, Integer amount) {
-        DetailsRequest request = new DetailsRequest();
-        String id = UUID.randomUUID().toString();
-        request.setRequestId(id);
-        DetailsResponse response = new DetailsResponse();
-        response.setDetails("ALFA 1234 1234 1234 1234");
-        response.setMerchant(Merchant.ALFA_TEAM);
-        response.setMerchantOrderStatus("PENDING");
-        response.setAmount(5000);
-        when(merchantDetailsService.getDetails(request)).thenReturn(Optional.of(response));
-        listener.receive(request);
-        ArgumentCaptor<DetailsResponse> captor = ArgumentCaptor.forClass(DetailsResponse.class);
-        verify(kafkaTemplate).send(eq("merchant-details-request-topic"), eq(id), captor.capture());
-        DetailsResponse actual = captor.getValue();
-        assertAll(
-                () -> assertEquals(id, actual.getRequestId()),
-                () -> assertEquals(details, actual.getDetails()),
-                () -> assertEquals(merchant, actual.getMerchant()),
-                () -> assertEquals(status, actual.getMerchantOrderStatus()),
-                () -> assertEquals(amount, actual.getAmount())
-        );
+    void receiveShouldCallServiceMethod() {
+        DetailsRequest detailsRequest = new DetailsRequest();
+        detailsRequest.setRequestId(UUID.randomUUID().toString());
+        detailsRequestTopicListener.receive(detailsRequest);
+        verify(detailsRequestProcessorService).process(detailsRequest);
     }
 }
