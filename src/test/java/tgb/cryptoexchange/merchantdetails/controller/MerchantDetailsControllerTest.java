0786b6644b2514f8afec59810b54128698dde7bf
barilo <e.barilo@isida.by>
2025-12-22 18:54:05 +0300

    версии API для получения конфигурации

--- src/test/java/tgb/cryptoexchange/merchantdetails/controller/MerchantDetailsControllerTest.java
+++ src/test/java/tgb/cryptoexchange/merchantdetails/controller/MerchantDetailsControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -19,9 +20,11 @@ import tgb.cryptoexchange.commons.enums.Merchant;
 import tgb.cryptoexchange.merchantdetails.constants.VariableType;
 import tgb.cryptoexchange.merchantdetails.details.CancelOrderRequest;
 import tgb.cryptoexchange.merchantdetails.dto.MerchantConfigDTO;
+import tgb.cryptoexchange.merchantdetails.dto.MerchantConfigRequest;
 import tgb.cryptoexchange.merchantdetails.dto.UpdateMerchantConfigDTO;
 import tgb.cryptoexchange.merchantdetails.entity.MerchantConfig;
 import tgb.cryptoexchange.merchantdetails.entity.Variable;
+import tgb.cryptoexchange.merchantdetails.service.MerchantApiService;
 import tgb.cryptoexchange.merchantdetails.service.MerchantConfigService;
 import tgb.cryptoexchange.merchantdetails.service.MerchantDetailsService;
 import tgb.cryptoexchange.merchantdetails.service.VariableService;
@@ -49,6 +52,9 @@ class MerchantDetailsControllerTest {
 
     @MockitoBean
     private VariableService variableService;
+
+    @MockitoBean
+    private MerchantApiService merchantApiService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -115,6 +121,48 @@ class MerchantDetailsControllerTest {
                 .andExpect(jsonPath("data[1]").exists())
                 .andExpect(jsonPath("data[2]").doesNotExist());
     }
+
+    @ValueSource(strings = {"0.10.0", "0.10.1", "1.0.0", "0.11.0"})
+    @ParameterizedTest
+    void getDetailsShouldAddMerchantsByApiVersion091IfNoHeader(String version) throws Exception {
+        List<MerchantConfigDTO> configs = new ArrayList<>();
+        configs.add(MerchantConfigDTO.fromEntity(MerchantConfig.builder()
+                .id(150L)
+                .merchant(Merchant.ALFA_TEAM)
+                .isOn(true)
+                .minAmount(100)
+                .maxAmount(200)
+                .merchantOrder(5)
+                .isAutoWithdrawalOn(true)
+                .build()));
+        configs.add(MerchantConfigDTO.fromEntity(MerchantConfig.builder()
+                .id(152346L)
+                .merchant(Merchant.WELL_BIT)
+                .isOn(false)
+                .minAmount(150)
+                .maxAmount(5000)
+                .merchantOrder(2)
+                .isAutoWithdrawalOn(true)
+                .build()));
+        Page<MerchantConfigDTO> page = new PageImpl<>(configs, Mockito.mock(Pageable.class), 2);
+        when(merchantConfigService.findAll(any(Pageable.class), any())).thenReturn(page);
+        List<Merchant> merchants = List.of(Merchant.ALFA_TEAM, Merchant.WELL_BIT);
+        when(merchantApiService.getMerchantsByApiVersion(any())).thenReturn(merchants);
+        mockMvc.perform(get("/merchant-details/config")
+                        .header("API-Version", version))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("success").value(true))
+                .andExpect(jsonPath("data").isArray())
+                .andExpect(jsonPath("data[0]").exists())
+                .andExpect(jsonPath("data[1]").exists())
+                .andExpect(jsonPath("data[2]").doesNotExist());
+        ArgumentCaptor<MerchantConfigRequest> requestCaptor = ArgumentCaptor.forClass(MerchantConfigRequest.class);
+        verify(merchantConfigService).findAll(any(Pageable.class), requestCaptor.capture());
+        assertEquals(requestCaptor.getValue().getMerchants(), merchants);
+        verify(merchantApiService).getMerchantsByApiVersion(version);
+    }
+
+
 
     @Test
     void updateConfigShouldCallServiceUpdateMethod() throws Exception {
