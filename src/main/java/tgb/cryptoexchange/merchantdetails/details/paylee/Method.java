0487de8dec61ba515294523f21a0217c960df79b
barilo <e.barilo@isida.by>
2025-12-24 16:51:22 +0300

    прием QR от PayLee

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/paylee/Method.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/paylee/Method.java
@@ -1,6 +1,9 @@
 package tgb.cryptoexchange.merchantdetails.details.paylee;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import lombok.AllArgsConstructor;
@@ -21,10 +24,27 @@ public enum Method implements MerchantMethod {
 
     private final String description;
 
+    public static Method fromValue(String value) {
+        for (Method method : Method.values()) {
+            if (method.getValue().equals(value)) {
+                return method;
+            }
+        }
+        return null;
+    }
+
     public static class Serializer extends JsonSerializer<Method> {
         @Override
         public void serialize(Method payLeeMethod, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
             jsonGenerator.writeString(payLeeMethod.getValue());
+        }
+    }
+
+    public static class Deserializer extends JsonDeserializer<Method> {
+
+        @Override
+        public Method deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            return Method.fromValue(jsonParser.getText());
         }
     }
 }
