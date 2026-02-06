8fb714754032995f42f0cea92ded604464df2625
barilo <e.barilo@isida.by>
2026-02-06 14:59:29 +0300

    метрики по конкретным мерчантам

--- src/main/java/tgb/cryptoexchange/merchantdetails/constants/Metrics.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/constants/Metrics.java
@@ -5,4 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Metrics {
     public static final String GET_DETAILS = "merchant_details_get_details";
+    public static final String GET_DETAILS_RESULT = "merchant_details_get_details_result";
+    public static final String MERCHANT_GET_DETAILS = "merchant_details_merchant_get_details";
+    public static final String MERCHANT_RESULT = "merchant_details_merchant_result";
 }
