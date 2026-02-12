32ee0dee3277fe4f4877cb7a01d1d44e28024fdb
vdovin <V.Vdovin@isida.by>
2026-02-12 13:00:36 +0300

    fix studio Callback add jsonProperty

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/studio/Callback.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/studio/Callback.java
@@ -1,5 +1,6 @@
 package tgb.cryptoexchange.merchantdetails.details.studio;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,6 +10,7 @@ import tgb.cryptoexchange.merchantdetails.details.UnwrappedCallback;
 @Data
 public class Callback extends UnwrappedCallback {
 
+    @JsonProperty("internal_id")
     private String id;
 
     @JsonDeserialize(using = Status.Deserializer.class)
