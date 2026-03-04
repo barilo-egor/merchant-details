f86a7459996efb585844690f967fbf0130d88296
GrewGh <grew.gh@mail.ru>
2026-03-04 13:55:58 +0300

    fix yolo store-key/credentials

--- src/main/java/tgb/cryptoexchange/merchantdetails/properties/YoloProperties.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/properties/YoloProperties.java
@@ -3,10 +3,9 @@ package tgb.cryptoexchange.merchantdetails.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("yolo")
-public record YoloProperties(String accountId, String url, String secretKey, Credentials credentials) {
+public record YoloProperties(String accountId, String url, String storeKey, Credentials credentials) {
     public record Credentials(
             String login,
-            String verificationCode,
             String passphrase
     ) {
     }
