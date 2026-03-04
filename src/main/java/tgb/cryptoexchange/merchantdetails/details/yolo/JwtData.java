b0469e89fa331e5e03fee94b7d1384b7982d275c
GrewGh <grew.gh@mail.ru>
2026-03-04 15:47:04 +0300

    fix yolo expiredAt заменен на Instant

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/yolo/JwtData.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/yolo/JwtData.java
@@ -1,16 +1,14 @@
 package tgb.cryptoexchange.merchantdetails.details.yolo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Data
 public class JwtData {
 
     private String accessToken;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime expiresAt;
+    private Instant expiresAt;
 
 }
