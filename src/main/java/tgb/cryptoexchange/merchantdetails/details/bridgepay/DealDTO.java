f32b2c8f2abe073b8cc7f704fd0755791f0b9e1a
GrewGh <grew.gh@mail.ru>
2026-03-05 13:25:35 +0300

    Добавлен мерчант AlfaTeamQR

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/bridgepay/DealDTO.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/bridgepay/DealDTO.java
@@ -1,12 +1,20 @@
 package tgb.cryptoexchange.merchantdetails.details.bridgepay;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DealDTO {
+
     @JsonDeserialize(using = Bank.Deserializer.class)
     private Bank paymentMethod;
 
+    @JsonDeserialize(using = Method.Deserializer.class)
+    private Method paymentOption;
+
     private RequisitesDTO requisites;
+
+    private String qrCodeLink;
 }
