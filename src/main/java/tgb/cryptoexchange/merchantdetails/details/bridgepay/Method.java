5629fc67fd2c0dcb05eab31583651aafe3b74131
GrewGh <grew.gh@mail.ru>
2026-03-05 13:25:35 +0300

    Добавлен мерчант AlfaTeamQR

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/bridgepay/Method.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/bridgepay/Method.java
@@ -1,6 +1,9 @@
 package tgb.cryptoexchange.merchantdetails.details.bridgepay;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import lombok.AllArgsConstructor;
@@ -15,7 +18,8 @@ public enum Method implements MerchantMethod {
     SBP("SBP", "СБП"),
     TO_CARD("TO_CARD", "Перевод на карту"),
     CROSS_BORDER("CROSS_BORDER", "Трансграничный перевод"),
-    MOBILE_TOP_UP("MOBILE_TOP_UP", "Пополнение счета моб.телефона");
+    MOBILE_TOP_UP("MOBILE_TOP_UP", "Пополнение счета моб.телефона"),
+    SBP_QR("SBP_QR", "СБП QR");
 
     private final String value;
 
@@ -25,6 +29,22 @@ public enum Method implements MerchantMethod {
         @Override
         public void serialize(Method method, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
             jsonGenerator.writeString(method.getValue());
+        }
+    }
+
+    private static Method fromValue(String value) {
+        for (Method method : Method.values()) {
+            if (method.getValue().equals(value)) {
+                return method;
+            }
+        }
+        return null;
+    }
+
+    public static class Deserializer extends JsonDeserializer<Method> {
+        @Override
+        public Method deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            return Method.fromValue(jsonParser.getValueAsString());
         }
     }
 }
