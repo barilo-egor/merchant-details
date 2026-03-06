8919e9de75eece3024fa6a6054754637e196b46d
barilo <e.barilo@isida.by>
2026-03-06 11:54:19 +0300

    сообщения для тестов

--- src/test/java/tgb/cryptoexchange/merchantdetails/constants/MerchantConstantsTest.java
+++ src/test/java/tgb/cryptoexchange/merchantdetails/constants/MerchantConstantsTest.java
@@ -10,11 +10,16 @@ class MerchantConstantsTest {
     @Test
     void allMerchantsMethodsAndStatusesShouldPresent() {
         for (Merchant merchant: Merchant.values()) {
-            assertDoesNotThrow(() -> MerchantConstants.getStatuses(merchant));
-            assertNotNull(MerchantConstants.getStatuses(merchant));
-            assertNotNull(MerchantConstants.getMethods(merchant));
-            assertNotEquals(0, MerchantConstants.getStatuses(merchant).size());
-            assertNotEquals(0, MerchantConstants.getMethods(merchant).size());
+            assertDoesNotThrow(() -> MerchantConstants.getStatuses(merchant),
+                    "For merchant " + merchant + " getStatuses throws exception.");
+            assertNotNull(MerchantConstants.getStatuses(merchant),
+                    "For merchant " + merchant + " getStatuses is null.");
+            assertNotNull(MerchantConstants.getMethods(merchant),
+                    "For merchant " + merchant + " getMethods is null.");
+            assertNotEquals(0, MerchantConstants.getStatuses(merchant).size(),
+                    "For merchant " + merchant + " getStatuses has size 0.");
+            assertNotEquals(0, MerchantConstants.getMethods(merchant).size(),
+                    "For merchant " + merchant + " getMethods has size 0.");
         }
     }
 
