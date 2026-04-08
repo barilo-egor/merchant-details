08c935b4cc4c10b956034d0f25c58e68fba23639
vdovin <V.Vdovin@isida.by>
2026-04-08 11:26:51 +0300

    fix

--- src/main/java/tgb/cryptoexchange/merchantdetails/service/StartupClearMerchantConfigService.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/service/StartupClearMerchantConfigService.java
@@ -3,13 +3,9 @@ package tgb.cryptoexchange.merchantdetails.service;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import tgb.cryptoexchange.commons.enums.Merchant;
-
-import java.util.List;
 
 @Component
-@Profile("!test")
+@Profile("!test & clear-merchants")
 public class StartupClearMerchantConfigService implements CommandLineRunner {
 
     private final MerchantConfigService merchantConfigService;
