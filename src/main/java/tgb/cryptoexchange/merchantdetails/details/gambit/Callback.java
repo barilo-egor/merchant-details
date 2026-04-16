e8fc3bd1faea8b19cf6670b3c8938f6b94a45662
vdovin <V.Vdovin@isida.by>
2026-04-16 10:44:04 +0300

    GAMBIT fix

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/gambit/Callback.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/gambit/Callback.java
@@ -1,34 +1,18 @@
 package tgb.cryptoexchange.merchantdetails.details.gambit;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import tgb.cryptoexchange.merchantdetails.details.MerchantOrderStatus;
 import tgb.cryptoexchange.merchantdetails.details.UnwrappedCallback;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class Callback extends UnwrappedCallback {
 
-    private CallbackData data;
-
-    @Override
-    public String getId() {
-        return data.getId();
-    }
-
-    @Override
-    public MerchantOrderStatus getStatus() {
-        return data.getStatus();
-    }
-
-    @Data
-    public static class CallbackData {
-
-        @JsonProperty("merchant_order_id")
-        private String id;
-
-        private Status status;
-    }
+    @JsonDeserialize(using = Status.Deserializer.class)
+    public Status status;
+    @JsonProperty("session_id")
+    private String id;
 
 }
