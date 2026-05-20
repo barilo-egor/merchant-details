9dce6be5a1ee24d9a99d97fb764a97dc2a4a0b24
vdovin <V.Vdovin@isida.by>
2026-05-20 13:53:31 +0300

    мерчант EclipseGate

---
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/eclipsegate/Response.java
@@ -0,0 +1,62 @@
+package tgb.cryptoexchange.merchantdetails.details.eclipsegate;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.Data;
+import tgb.cryptoexchange.merchantdetails.details.MerchantDetailsResponse;
+import tgb.cryptoexchange.merchantdetails.details.ValidationResult;
+
+import java.util.Objects;
+
+@Data
+public class Response implements MerchantDetailsResponse {
+
+    @JsonProperty("gate_id")
+    private String orderId;
+
+    @JsonDeserialize(using = Status.Deserializer.class)
+    private Status status;
+
+    private Integer amount;
+
+    private Requisites requisites;
+
+    @Override
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult();
+        if (hasDetails()) {
+            if (Objects.isNull(orderId)) {
+                result.notNull("orderId");
+            }
+            if (Objects.isNull(status)) {
+                result.notNull("status");
+            }
+            if (Objects.isNull(amount)) {
+                result.notNull("amount");
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean hasDetails() {
+        return Objects.nonNull(requisites) && Objects.nonNull(requisites.bankName)
+                && (Objects.nonNull(requisites.getBill()) || Objects.nonNull(requisites.getPhone()));
+    }
+
+    @Data
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class Requisites {
+
+        @JsonProperty("bank_name")
+        private String bankName;
+
+        private String phone;
+
+        private String bill;
+
+    }
+
+}
+
