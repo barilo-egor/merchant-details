40cebcc2e36a13c721d18440bce97f90caae3647
vdovin <V.Vdovin@isida.by>
2026-05-26 11:18:17 +0300

    Meridian pay возвращает сумму в копейках

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/levelpay/MeridianPayHighCheckOrderCreationService.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/levelpay/MeridianPayHighCheckOrderCreationService.java
@@ -17,6 +17,11 @@ public class MeridianPayHighCheckOrderCreationService extends LevelPayOrderCreat
     }
 
     @Override
+    protected Integer getAmount(String amount) {
+        return Integer.parseInt(amount) / 100;
+    }
+
+    @Override
     public Merchant getMerchant() {
         return Merchant.MERIDIAN_PAY_HIGH_CHECK;
     }
--- src/main/java/tgb/cryptoexchange/merchantdetails/details/levelpay/MeridianPayOrderCreationService.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/levelpay/MeridianPayOrderCreationService.java
@@ -17,6 +17,11 @@ public class MeridianPayOrderCreationService extends LevelPayOrderCreationServic
     }
 
     @Override
+    protected Integer getAmount(String amount) {
+        return Integer.parseInt(amount) / 100;
+    }
+
+    @Override
     public Merchant getMerchant() {
         return Merchant.MERIDIAN_PAY;
     }
