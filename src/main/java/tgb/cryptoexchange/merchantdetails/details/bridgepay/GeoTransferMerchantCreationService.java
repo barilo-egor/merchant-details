92a2362617ebf895b877d2ee3af58f1d61de6ff3
GrewGh <grew.gh@mail.ru>
2026-06-14 21:00:28 +0300

    GeoTransfer qr method

--- src/main/java/tgb/cryptoexchange/merchantdetails/details/bridgepay/GeoTransferMerchantCreationService.java
+++ src/main/java/tgb/cryptoexchange/merchantdetails/details/bridgepay/GeoTransferMerchantCreationService.java
@@ -8,13 +8,28 @@ import tgb.cryptoexchange.merchantdetails.config.CallbackConfig;
 import tgb.cryptoexchange.merchantdetails.properties.GeoTransferProperties;
 import tgb.cryptoexchange.merchantdetails.service.SignatureService;
 
+import java.util.function.Function;
+
 @Service
 public class GeoTransferMerchantCreationService extends BridgePayOrderCreationService {
+
+    private final GeoTransferProperties geoTransferProperties;
 
     protected GeoTransferMerchantCreationService(@Qualifier("geoTransferWebClient") WebClient webClient,
                                                  GeoTransferProperties geoTransferProperties, SignatureService signatureService,
                                                  CallbackConfig callbackConfig) {
         super(webClient, geoTransferProperties, signatureService, callbackConfig);
+        this.geoTransferProperties = geoTransferProperties;
+    }
+
+    @Override
+    protected Function<Method, String> keyFunction() {
+        return method -> {
+            if (Method.SBP_QR.equals(method)) {
+                return geoTransferProperties.keyQr();
+            }
+            return geoTransferProperties.key();
+        };
     }
 
     @Override
